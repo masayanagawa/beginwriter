dac11f13da8d6a9b7bda8e5bd2acab639771b134
梁川将和 <yanagawamasakazu@yanagawama-2.local>
2019-07-08 16:59:52 +0900

    edit ignore

--- plugins/amplify_init.js
+++ plugins/amplify_init.js
@@ -1,10 +1,13 @@
 import Amplify, { Auth } from 'aws-amplify'
+let region =  `${process.env.AMPLIFY_AUTH_REGION}`
+let userPoolId = `${AMPLIFY_AUTH_USER_POOL_ID}`
+let userPoolWebClientId = `${process.env.AMPLIFY_AUTH_USER_POOL_WEB_CLIENT_ID}`
 export default async () => {
   Amplify.configure({
     Auth: {
-      "region": 'ap-northeast-1',
-      "userPoolId": 'ap-northeast-1_eGGJctmO3',
-      "userPoolWebClientId": '7df08tmuii8o4lec0cpk752sin'
+      "region": region,
+      "userPoolId": userPoolId,
+      "userPoolWebClientId": userPoolWebClientId
     }
   })
 }
