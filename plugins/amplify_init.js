23735d6f202d865c55f0caf79f144d737f36c35a
梁川将和 <yanagawamasakazu@yanagawama-2.local>
2019-07-08 17:29:32 +0900

    edit amplify

--- plugins/amplify_init.js
+++ plugins/amplify_init.js
@@ -1,7 +1,4 @@
 import Amplify, { Auth } from 'aws-amplify'
-let region =  process.env.AMPLIFY_AUTH_REGION
-let userPoolId = process.env.AMPLIFY_AUTH_USER_POOL_ID
-let userPoolWebClientId = process.env.AMPLIFY_AUTH_USER_POOL_WEB_CLIENT_ID
 export default async () => {
   Amplify.configure({
     Auth: {
